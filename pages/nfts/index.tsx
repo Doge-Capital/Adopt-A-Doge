f068ab267d2f89428b1893636ea2a6ae5bffa642
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-09-24 20:31:28 +0530

    changes

--- pages/nfts/index.tsx
+++ pages/nfts/index.tsx
@@ -25,16 +25,10 @@ function EligibleNfts() {
         <h3 className="text-center mt-4 font-semibold text-[1.625rem] opacity-75 font-bold">
           Your Eligible NFTs
         </h3>
-        <div className="h-screen flex gap-8">
-          <div className="">
-            <Nftcard />
-          </div>
-          <div className="">
-            <Nftcard />
-          </div>
-          <div className="">
-            <Nftcard />
-          </div>
+        <div className="h-screen ">
+         <NFtDisplay />
+{/* <WalletNotConnected /> */}
+{/* <NoELigibeNfts /> */}
         </div>
       </div>
       <div className="h-[20vh] fixed bottom-0 w-full px-12 bg-bg">
@@ -43,9 +37,13 @@ function EligibleNfts() {
             <h3>NFTs Selected : 00</h3>
             <h3 className="mx-12">Tickets Received : 00</h3>
           </div>
-          <button className="flex ml-auto mr-0 mt-4 text-lg bg-grey rounded-sm text-white px-2">
+          {/* <button className="flex ml-auto mr-0 mt-4 text-lg bg-grey rounded-sm text-white px-4 py-2">
+            Burn and receive tickets
+          </button> */}
+          <button className="flex ml-auto mr-0 mt-4 text-lg bg-[#2278F9] rounded-sm text-white px-4 py-2">
             Burn and receive tickets
           </button>
+
         </div>
       </div>
     </div>
@@ -53,3 +51,38 @@ function EligibleNfts() {
 }
 
 export default EligibleNfts;
+
+
+const WalletNotConnected = () => {
+  return(
+    <div className="flex text-center mx-auto w-fit mt-20 ">
+      <h3 className="text-[#898989] text-lg font-semibold">Connect wallet to display NFTs</h3>
+    </div>
+  )
+}
+
+
+const NoELigibeNfts = () => {
+  return(
+    <div className="flex text-center mx-auto w-fit mt-20">
+      <h3 className="text-[#D44C4C] text-lg  font-semibold">Oops.....You dont have any eligible NFTs</h3>
+    </div>
+  )
+  
+}
+
+const NFtDisplay = () => {
+  return(
+    <div className="flex gap-8 mt-4">
+       <div className="">
+            <Nftcard />
+          </div>
+          <div className="">
+            <Nftcard />
+          </div>
+          <div className="">
+            <Nftcard />
+          </div>
+    </div>
+  )
+}
