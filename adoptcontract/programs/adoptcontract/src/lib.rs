3759487dc24cf93f27a4a9b3d9fa75da7bc2675c
ilyxabatko <ilyxabatko@inbox.ru>
2023-07-22 02:28:56 +0400

    Initial burn transaction and test for it

--- adoptcontract/programs/adoptcontract/src/lib.rs
+++ adoptcontract/programs/adoptcontract/src/lib.rs
@@ -1,15 +1,84 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, system_program};
+use anchor_spl::token::{self, Burn, Mint, Token, TokenAccount, CloseAccount};
 
-declare_id!("GntfZtKwsEZyygp35RM5ooBuHirNBCEdcnGSGByAgEtF");
+declare_id!("6yWqtzp77Z8CfNTrd4Kc8wdBUh8xrTvSPL1NDqdpNKKU");
 
 #[program]
 pub mod adoptcontract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    // Since we're gonna burn NFTs, we don't need to provide any bigger number for the "approve_token" ix per ATA
+    const ONE: u64 = 1;
+    const ADDITIONAL_TX_FEE: u64 = 5_000_000; // 0.005 SOL
+
+    pub fn burn_token(ctx: Context<BurnToken>) -> Result<()> {
+        // Burn a token
+        msg!("Burning NFT...");
+
+        let cpi_accounts = Burn {
+            mint: ctx.accounts.mint.to_account_info(),
+            from: ctx.accounts.from.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        // Execute anchor's helper function to burn tokens
+        token::burn(cpi_ctx, ONE)?;
+
+        // Charge additional fee
+        msg!("Transferring additional fee...");
+        
+        let cpi_accounts_fee = system_program::Transfer {
+            from: ctx.accounts.authority.to_account_info().clone(),
+            to: ctx.accounts.fees_receiver.to_account_info().clone()
+        };
+        let cpi_program_fee = ctx.accounts.system_program.to_account_info();
+        let cpi_ctx_fee = CpiContext::new(cpi_program_fee, cpi_accounts_fee);
+        system_program::transfer(cpi_ctx_fee, ADDITIONAL_TX_FEE)?;
+        
+        // Closes token account and sends rent SOL to a particular account
+        msg!("Closing TA...");
+        let cpi_accounts_fee = CloseAccount {
+            account: ctx.accounts.from.to_account_info().clone(),
+            destination: ctx.accounts.fees_receiver.to_account_info().clone(),
+            authority: ctx.accounts.authority.to_account_info().clone()
+        };
+        let cpi_program_fee = ctx.accounts.system_program.to_account_info();
+        let cpi_ctx_fee = CpiContext::new(cpi_program_fee, cpi_accounts_fee);
+        token::close_account(cpi_ctx_fee)?;
+
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct BurnToken<'info> {
+    #[account(
+        mut,
+        mint::decimals = 0
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = authority
+    )]
+    pub from: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    /// CHECK: we'll add a particular hard-coded address to the constraints
+    #[account(
+        mut,
+        // TODO!  -  address = FEES_RECEIVER_ADDRESS
+    )]
+    pub fees_receiver: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("")]
+    ErrorName
+}
--- adoptcontract/tests/adoptcontract.ts
+++ adoptcontract/tests/adoptcontract.ts
@@ -1,16 +1,101 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { TOKEN_PROGRAM_ID, createMint, createAssociatedTokenAccount, mintToChecked, getMint, getAccount } from "@solana/spl-token";
 import { Adoptcontract } from "../target/types/adoptcontract";
+import { expect } from "chai";
 
 describe("adoptcontract", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    const provider = anchor.AnchorProvider.env();
+    const connection = provider.connection;
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.Adoptcontract as Program<Adoptcontract>;
+    const program = anchor.workspace.Adoptcontract as Program<Adoptcontract>;
+    let test_keypair: anchor.web3.Keypair;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const mintToken = async (): Promise<[anchor.web3.PublicKey, anchor.web3.PublicKey]> => {
+        // Create mint
+        let mintPubkey = await createMint(
+            connection, // conneciton
+            test_keypair, // fee payer
+            test_keypair.publicKey, // mint authority
+            test_keypair.publicKey, // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
+            0 // decimals
+        );
+
+        let mintAccount = await getMint(connection, mintPubkey);
+        console.log("Mint account: " + mintAccount.address);
+
+        // Create the token account
+        let ata = await createAssociatedTokenAccount(
+            connection, // connection
+            test_keypair, // fee payer
+            mintPubkey, // mint
+            test_keypair.publicKey // owner,
+        );
+
+        let tokenAccount = await getAccount(connection, ata);
+        console.log(`ATA address: ${tokenAccount.address}\nATA mint: ${tokenAccount.mint}\nATA owner: ${tokenAccount.owner}`);
+
+
+        // Mint token to the account
+        let txhash = await mintToChecked(
+            connection, // connection
+            test_keypair, // fee payer
+            mintPubkey, // mint
+            ata, // receiver (sholud be an associated token account)
+            test_keypair.publicKey, // mint authority
+            1, // amount
+            0 // decimals
+        );
+
+        console.log(`Token was minted, tx hash: ${txhash}`);
+
+        return [mintPubkey, ata];
+    };
+
+    before("Generate keypair, airdrop and mint a 0-decimal token", async function () {
+        test_keypair = anchor.web3.Keypair.generate();
+
+        let airdrop_txhash = await connection.requestAirdrop(test_keypair.publicKey, .5 * anchor.web3.LAMPORTS_PER_SOL).catch(console.error);
+        const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+
+        airdrop_txhash && await connection.confirmTransaction({
+            signature: airdrop_txhash,
+            blockhash,
+            lastValidBlockHeight
+        }, "finalized");
+
+        console.log(`Airdrop tx hash: ${airdrop_txhash}`);
+    });
+
+    it("burns an NFT and closes its TA", async () => {
+        try {
+            console.log("Burn NFT instruction...");
+            const [mintPublicKey, ata] = await mintToken();
+
+            await connection.getTokenAccountBalance(ata).then(balance => {
+                console.log("Token amount before burning: " + balance.value.amount);
+                expect(balance.value.amount).eq("1", "Wrong token amount, should be 1");
+            });
+            
+            // Add your test here.
+            const tx = await program.methods.burnToken()
+            .accounts({
+                mint: mintPublicKey,
+                from: ata,
+                authority: test_keypair.publicKey,
+                feesReceiver: test_keypair.publicKey,
+                tokenProgram: TOKEN_PROGRAM_ID,
+                systemProgram: anchor.web3.SystemProgram.programId
+            })
+            .signers([test_keypair])
+            .rpc();
+
+            // TODO: add an assertion (TA doesn't exist and mint has 0 supply)
+            
+            console.log("Burning transaction signature", tx);
+        } catch (error) {
+            console.log(error);
+        }
+    });
 });
