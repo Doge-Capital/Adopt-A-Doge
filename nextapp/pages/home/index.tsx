1a71e05c836c882fca5dc7344bce5b9693a7f7e3
ilyxabatko <ilyxabatko@inbox.ru>
2023-07-25 01:24:38 +0400

    landing page changes

--- nextapp/pages/home/index.tsx
+++ nextapp/pages/home/index.tsx
@@ -1,61 +1,112 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import { Image } from "@nextui-org/react";
-import Winners from "../../components/Winners";
-import  styles from '../../components/styles/animation.module.css'
+import styles from '../../components/styles/animation.module.css'
 function Home() {
-  return (
-    <div className="bg-[#F5F5F5] font-inter">
-      <Navbar />
-     <div className="pb-12 border-b-[1px] border-[#4C82D4] mx-12 mt-12">
-     <div className=" ">
-        <div className="flex justify-center items-center w-fit mx-auto">
-        <Image style={{
-          transform:'scaleX(-1)'
-        }} src="/assets/images/home4.png" width={80} className={styles['animated_image']} />
-        <h3 className="text-4xl font-bold text-center">
-          Earn with your rugged NFTs
-        </h3>
-        <Image src="/assets/images/home4.png" width={80} className={styles['animated_image']} />
+    return (
+        <div className="bg-[#F5F5F5] font-inter h-full">
+            <Navbar />
+
+            <div className="pb-12 mx-12 flex flex-col justify-center items-center">
+
+                <div className="w-screen bg-header-banner bg-no-repeat bg-center bg-contain h-60 flex flex-col justify-center">
+                    <div className="flex justify-center items-center w-fit mx-auto mb-5">
+                        <h3 className="text-4xl font-bold text-center">
+                            Earn with your rugged NFTs
+                        </h3>
+                    </div>
+                    {/* <h3 className="text-5xl font-bold text-center">
+                        Earn with your rugged NFTs
+                    </h3> */}
+                    <p className="opacity-50 font-semibold text-xl text-center max-w-[40ch] flex mx-auto">
+                        A simple & secure way for people to have a chance to earn with their
+                        rugged NFTs
+                    </p>
+                </div>
+
+                <div className="flex justify-center gap-7 mx-16 mt-12 mb-48">
+                    <div className="flex flex-col items-center text-center">
+                        <Image src="/assets/images/nft_gib_icon.svg" width={250} className='hover:scale-[1.05]' />
+                        <p className="text-lg font-medium">
+                            Give us your rugged NFTs
+                        </p>
+                        <p className="text-sm opacity-50 font-semibold mt-1">
+                            Small fee attatched with the transaction
+                        </p>
+                    </div>
+
+                    <Image src="/assets/images/right_arrow_icon.png" width={40} className='hover:scale-[1.05]' />
+
+                    <div className="flex flex-col items-center text-center">
+                        <Image src="/assets/images/ticket_icon.svg" width={250} className='hover:scale-[1.05]' />
+                        <p className="text-lg font-medium">
+                            Receive Tickets
+                        </p>
+                        <p className="text-sm opacity-50 font-semibold mt-1">
+                            Small fee attatched with the transaction
+                        </p>
+                    </div>
+
+                    <Image src="/assets/images/right_arrow_icon.png" width={40} className='hover:scale-[1.05]' />
+
+                    <div className="flex flex-col items-center text-center">
+                        <Image src="/assets/images/nft_token_icon.svg" width={250} className='hover:scale-[1.05]' />
+                        <p className="text-lg font-medium">
+                            Participate in raffles
+                        </p>
+                        <p className="text-sm opacity-50 font-semibold mt-1">
+                            Small fee attatched with the transaction
+                        </p>
+                    </div>
+                </div>
+
+                <div className="mx-16 mb-12">
+
+                    <p className="font-semibold text-xl text-center">
+                        What do we do with the fees collected?
+                    </p>
+
+                    <div className="flex justify-center mt-24 gap-5">
+                        <div className="flex flex-col items-center text-center w-60">
+                            <div className="rounded-full bg-slate-200 w-52 h-52 flex justify-center">
+                                <Image src="/assets/images/money_icon.svg" width={65} className='hover:scale-[1.05]' />
+                            </div>
+                            <p className="text-base opacity-50 font-semibold mt-3">
+                                We pull the collected fees
+                            </p>
+                        </div>
+
+                        <Image src="/assets/images/back_arrow_icon.svg" width={40} className='hover:scale-[1.05]' />
+
+                        <div className="flex flex-col items-center text-center w-60">
+                            <div className="rounded-full bg-slate-200 w-52 h-52 flex justify-center">
+                                <Image src="/assets/images/paw_icon.svg" width={85} className='hover:scale-[1.05]' />
+                            </div>
+                            <p className="text-base opacity-50 font-semibold mt-3">
+                                Buy floor Doge Capital NFTs
+                            </p>
+                        </div>
+
+                        <Image src="/assets/images/back_arrow_icon.svg" width={40} className='hover:scale-[1.05]' />
+
+                        <div className="flex flex-col items-center text-center w-60">
+                            <div className="rounded-full bg-slate-200 w-52 h-52 flex justify-center">
+                                <Image src="/assets/images/crown_icon.svg" width={110} className='hover:scale-[1.05]' />
+                            </div>
+                            <p className="text-base opacity-50 font-semibold mt-3">
+                                Create raffles for you
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="text-lg bg-[#2278F9] px-8 rounded-full text-white w-fit flex mx-auto py-2 hover:bg-[#4f8ff0] cursor-pointer	">
+                    Get Started
+                </div>
+
+            </div>
         </div>
-         {/* <h3 className="text-5xl font-bold text-center">
-          Earn with your rugged NFTs
-        </h3> */}
-        <p className="opacity-50 font-semibold text-xl text-center max-w-[40ch] flex mx-auto ">
-          A simple & secure way for people to have a chance to earn with their
-          rugged NFTs
-        </p>
-      </div>
-      <div className="flex justify-around mx-16 mt-12">
-        <div className="flex flex-col items-center text-center">
-          <Image src="/assets/images/home1.svg" width={250} className='hover:scale-[1.05]' />
-          <p className="text-lg font-medium">Give us your rugged NFTs</p>
-          <p className="text-sm opacity-50 font-semibold">
-            Small fee attatched with the transaction
-          </p>
-        </div>
-        <div className="flex flex-col items-center text-center">
-          <Image src="/assets/images/home3.svg" width={250} className='hover:scale-[1.05]'/>
-          <p className="text-lg font-medium">Give us your rugged NFTs</p>
-          <p className="text-sm opacity-50 font-semibold">
-            Small fee attatched with the transaction
-          </p>
-        </div>
-        <div className="flex flex-col items-center text-center">
-          <Image src="/assets/images/home2.svg"  width={250} className='hover:scale-[1.05]'/>
-          <p className="text-lg font-medium">Give us your rugged NFTs</p>
-          <p className="text-sm opacity-50 font-semibold">
-            Small fee attatched with the transaction
-          </p>
-        </div>
-      </div>
-      <div className="text-lg bg-[#2278F9] px-8 rounded-full text-white w-fit flex mx-auto mt-12 py-2 hover:bg-[#4f8ff0] cursor-pointer	">
-        Get Started
-      </div>
-     </div>
-     <Winners />
-    </div>
-  );
+    );
 }
 
 export default Home;
--- nextapp/tailwind.config.js
+++ nextapp/tailwind.config.js
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    
-    extend: {
-      colors: {
-        'blue': '#E6F5FB',
-        'green': '#EFFCEF',
-        'purple': '#F0F5FF',
-        'grey': '#B4B4B4',
-        'bg':'#F5F5F5'
-      },
-      borderRadius: {
-        'sm': '0.313rem',
-      },
-      fontFamily:{
-        'inter' : ['Inter', 'sans-serif'],
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'blue': '#E6F5FB',
+                'green': '#EFFCEF',
+                'purple': '#F0F5FF',
+                'grey': '#B4B4B4',
+                'bg': '#F5F5F5'
+            },
+            borderRadius: {
+                'sm': '0.313rem',
+            },
+            fontFamily: {
+                'inter': ['Inter', 'sans-serif'],
+            },
+            backgroundImage: {
+                'header-banner': "url('/assets/images/header_bg_banner.png')",
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
