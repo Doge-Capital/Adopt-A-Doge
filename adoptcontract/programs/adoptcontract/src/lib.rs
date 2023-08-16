f3a3fec6dd592bfee395d879dfc45c76f3dd254f
ilyxabatko <ilyxabatko@inbox.ru>
2023-08-16 14:32:54 +0400

    adds "nft transfer" ix from PDA and a test

--- adoptcontract/programs/adoptcontract/src/lib.rs
+++ adoptcontract/programs/adoptcontract/src/lib.rs
@@ -1,14 +1,14 @@
 use anchor_lang::{prelude::*, solana_program::program::invoke, system_program};
-use anchor_spl::token::Token;
+use anchor_spl::{token::{Token, TokenAccount, Transfer, transfer, Mint}, associated_token::AssociatedToken};
 use mpl_token_metadata::{instruction::burn_nft, ID as TOKEN_METADATA_PROGRAM_ID};
 
-declare_id!("DruXG4fVzPmdsfMqjNyrUaBv5aTvLnP9G43jemUcX1J1");
+declare_id!("WK2HDpKKafvGms55U4WgeD9RkgshRcHsddndja4ooeY");
 
 #[program]
 pub mod adoptcontract {
     use super::*;
 
-    const ADDITIONAL_TX_FEE: u64 = 2_000_000; // 0.002 SOL per 1 NFT burn
+    const ADDITIONAL_TX_FEE: u64 = 100_000_000; // 0.1 SOL per 1 NFT burn
     const FOUR: u64 = 4; // 4 accs in a chunk of remaining accounts per NFT
 
     pub fn burn<'info>(ctx: Context<'_, '_, '_, 'info, BurnNFT<'info>>) -> Result<()> {
@@ -26,7 +26,7 @@ pub mod adoptcontract {
                 return err!(ErrorCode::WrongRemainingAccountsChunk);
             }
 
-            // we don't need to check all of them, because Metaplex 
+            // we don't need to check all of them, because Metaplex
             // will do that for us and return an error if the layout is wrong
             let mint_account = chunk[0];
             let metadata_account = chunk[1];
@@ -68,6 +68,39 @@ pub mod adoptcontract {
         let cpi_ctx_fee = CpiContext::new(cpi_program_fee, cpi_accounts_fee);
         system_program::transfer(cpi_ctx_fee, fee)?;
 
+        // Transfer a ticket
+
+        Ok(())
+    }
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let bump = *ctx.bumps.get("pda_account").ok_or(ErrorCode::CannotGetBump)?;
+        ctx.accounts.pda_account.bump_seed = bump;
+
+        Ok(())
+    }
+
+    pub fn transfer_nft_from_pda(ctx: Context<SendTicket>) -> Result<()> {
+        // bump check 
+        let bump = *ctx.bumps.get("authority").ok_or(ErrorCode::CannotGetBump)?;
+        assert_eq!(bump, ctx.accounts.authority.bump_seed);
+
+        // creates an array of seeds to sign on behalf of the PDA
+        let seeds = &["authority".as_bytes(), &[*ctx.bumps.get("authority").unwrap()]];
+        let signer = [&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                from: ctx.accounts.ata_to_send_from.to_account_info(),
+                to: ctx.accounts.ata_to_receive_ticket.to_account_info(),
+                authority: ctx.accounts.authority.to_account_info(),
+            },
+            &signer,
+        );
+
+        transfer(cpi_ctx, 1)?;
+
         Ok(())
     }
 }
@@ -90,8 +123,71 @@ pub struct BurnNFT<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        seeds = [b"authority".as_ref()],
+        space = NftsValutAccount::SIZE,
+        bump,
+        payer = signer,
+    )]
+    pub pda_account: Account<'info, NftsValutAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SendTicket<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    /// CHECK: constraint checks
+    #[account(
+        mut,
+        seeds = [b"authority".as_ref()],
+        bump
+        // address = "address" @ WrongPDAAddress
+    )]
+    pub authority: Account<'info, NftsValutAccount>,
+    #[account(
+        mut,
+        token::mint = mint,
+        token::authority = authority
+    )]
+    pub ata_to_send_from: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        mint::decimals = 0
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = payer
+    )]
+    pub ata_to_receive_ticket: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[account]
+pub struct NftsValutAccount {
+    pub bump_seed: u8,
+}
+impl NftsValutAccount {
+    const SIZE: usize = 8 + 8; // 8 - descriminator, 8 - u64;
+}
+
 #[error_code]
 pub enum ErrorCode {
     #[msg("Wrong remaining accounts chunk (should be 4 in each one)")]
     WrongRemainingAccountsChunk,
+    #[msg("Wrong PDA address, must be 'address'.")]
+    WrongPDAAddress,
+    #[msg("Cannot get the bump of the Vault PDA")]
+    CannotGetBump
 }
--- adoptcontract/tests/adoptcontract.ts
+++ adoptcontract/tests/adoptcontract.ts
@@ -1,9 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { TOKEN_PROGRAM_ID, getAssociatedTokenAddress } from "@solana/spl-token";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, createTransferInstruction, getAssociatedTokenAddress, transfer } from "@solana/spl-token";
 import { Metaplex, keypairIdentity, Nft } from "@metaplex-foundation/js";
 import { Adoptcontract } from "../target/types/adoptcontract";
-import { expect } from "chai";
 import { AccountMeta } from "@solana/web3.js";
 
 describe("adoptcontract", () => {
@@ -36,12 +35,53 @@ describe("adoptcontract", () => {
         return [nft, nftATA];
     };
 
+    const mintNftToPda = async (): Promise<[Nft, anchor.web3.PublicKey]> => {
+        const valutPda = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                anchor.utils.bytes.utf8.encode("authority")
+            ],
+            program.programId
+        )[0];
+
+        console.log("Minting an NFT...");
+        const { nft } = await metaplex.nfts().create({
+            uri: "",
+            name: "My NFT",
+            sellerFeeBasisPoints: 500, // Represents 5.00%.
+        });
+
+        console.log("NFT created (mint): " + nft.address + "\n" + "NFT name: " + nft.name + "\n" + "NFT Metadata: " + nft.metadataAddress + "\n" + "NFT Edition: " + nft.edition.address);
+
+        const nftATA = await getAssociatedTokenAddress(nft.address, valutPda, true);
+        const myATA = await getAssociatedTokenAddress(nft.address, test_keypair.publicKey);
+
+        const send_tx = await metaplex.nfts().transfer({
+            nftOrSft: nft,
+            authority: test_keypair,
+            fromOwner: test_keypair.publicKey,
+            fromToken: myATA,
+            toOwner: valutPda,
+            toToken: nftATA,
+        }, { commitment: "finalized" });
+
+        console.log(`Transfer to PDA transaction signature: ${await send_tx.response.signature}`);
+
+        const updatedNft = await metaplex.nfts().findByMint({
+            mintAddress: nft.address,
+            tokenOwner: valutPda
+        }) as Nft;
+
+        console.log("NFT Token Account (PDA TA): " + nftATA);
+
+        return [updatedNft, nftATA];
+    };
+
     const createAccountMeta = (mint: anchor.web3.PublicKey, metadata: anchor.web3.PublicKey, edition: anchor.web3.PublicKey, ata: anchor.web3.PublicKey): Array<AccountMeta> => {
         return [
-            {pubkey: mint, isWritable: true, isSigner: false},
-            {pubkey: metadata, isWritable: true, isSigner: false},
-            {pubkey: edition, isWritable: true, isSigner: false},
-            {pubkey: ata, isWritable: true, isSigner: false},
+            { pubkey: mint, isWritable: true, isSigner: false },
+            { pubkey: metadata, isWritable: true, isSigner: false },
+            { pubkey: edition, isWritable: true, isSigner: false },
+            { pubkey: ata, isWritable: true, isSigner: false },
         ];
     };
 
@@ -61,7 +101,7 @@ describe("adoptcontract", () => {
         console.log(`Airdrop tx hash: ${airdrop_txhash}`);
     });
 
-    it("burns 4 NFTs and transfers fees", async () => {
+    it.skip("burns 4 NFTs and transfers fees", async () => {
         const [nft1, ata1] = await mintToken();
         const [nft2, ata2] = await mintToken();
         const [nft3, ata3] = await mintToken();
@@ -73,7 +113,6 @@ describe("adoptcontract", () => {
         let mint4AccountMeta = createAccountMeta(nft4.address, nft4.metadataAddress, nft4.edition.address, ata4);
 
         let combinedAccountMeta = [...mint1AccountMeta, ...mint2AccountMeta, ...mint3AccountMeta, ...mint4AccountMeta];
-        
 
         console.log("Runs \"burn\" ix...");
         const tx = await program.methods.burn()
@@ -90,9 +129,163 @@ describe("adoptcontract", () => {
                 skipPreflight: true
             });
 
-
         console.log("Burning transaction signature", tx);
 
         // TODO: add an assertion (TA doesn't exist and mint has 0 supply)
     });
+
+    // Set "it.skip()" in case you have a PDA initialized already
+    it.skip("initializes the PDA account", async () => {
+        console.log("Initializing PDA...");
+
+        const vaultPDA = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                anchor.utils.bytes.utf8.encode("authority"),
+            ],
+            program.programId
+        )[0];
+
+        try {
+            const tx = await program.methods.initialize()
+                .accounts({
+                    signer: test_keypair.publicKey,
+                    pdaAccount: vaultPDA,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                })
+                .signers([test_keypair])
+                .rpc({
+                    skipPreflight: true
+                });
+
+            console.log("Init transaction signature", tx);
+        } catch (error) {
+            console.log("An error occurred initializing the PDA account: ", error);
+        }
+    })
+
+    it.skip("sends a token (ticket) from the PDA", async () => {
+        // uncomment this in case you already initialized the PDA account and comment the "findProgramAddressSync" method
+        const vaultPDA = "1XKyJWhajCumUmPcqt3VxARoK2CuTx9ZqChNjN2u5aJ";
+
+        // const vaultPDA = anchor.web3.PublicKey.findProgramAddressSync(
+        //     [
+        //         anchor.utils.bytes.utf8.encode("authority"),
+        //     ],
+        //     program.programId
+        // )[0];
+
+        console.log("Minting NFT and transferring to PDA...")
+
+
+
+        try {
+            const [nft, pdaAta] = await mintNftToPda();
+
+            const myATA = await getAssociatedTokenAddress(nft.address, test_keypair.publicKey);
+
+            const tx2 = await program.methods.initialize()
+                .accounts({
+                    signer: test_keypair.publicKey,
+                    pdaAccount: vaultPDA,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                })
+                .signers([test_keypair])
+                .instruction();
+
+            const tx = await program.methods.transferNftFromPda()
+                .accounts({
+                    payer: test_keypair.publicKey,
+                    authority: vaultPDA,
+                    ataToSendFrom: pdaAta,
+                    mint: nft.mint.address,
+                    ataToReceiveTicket: myATA,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                    rent: anchor.web3.SYSVAR_RENT_PUBKEY
+                })
+                .signers([test_keypair])
+                .postInstructions([tx2])
+                .rpc({
+                    skipPreflight: true
+                });
+
+            console.log("Transfer transaction signature", tx);
+        } catch (error) {
+            console.log("An error occcured while minting and transferring NFT from PDA: ", error);
+        }
+    })
+
+    it("inits, burns and transfers a ticket", async () => {
+        const vaultPDA = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                anchor.utils.bytes.utf8.encode("authority"),
+            ],
+            program.programId
+        )[0];
+
+        try {
+            // INIT instruction, uncomment if you havent initialized a PDA yet and add that to the main burn instruction
+            // as "preInstruction([initTx])"
+
+            // const initIx = await program.methods.initialize()
+            //     .accounts({
+            //         signer: test_keypair.publicKey,
+            //         pdaAccount: vaultPDA,
+            //         systemProgram: anchor.web3.SystemProgram.programId,
+            //     })
+            //     .signers([test_keypair])
+            //     instruction();
+
+            // TRANSFER ticket instruction
+            const [nft, pdaAta] = await mintNftToPda();
+            const myATA = await getAssociatedTokenAddress(nft.address, test_keypair.publicKey);
+
+            const transferTicketIx = await program.methods.transferNftFromPda()
+                .accounts({
+                    payer: test_keypair.publicKey,
+                    authority: vaultPDA,
+                    ataToSendFrom: pdaAta,
+                    mint: nft.mint.address,
+                    ataToReceiveTicket: myATA,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                    rent: anchor.web3.SYSVAR_RENT_PUBKEY
+                })
+                .signers([test_keypair])
+                .instruction();
+
+            // main (BRUN) instruction
+            const [nft1, ata1] = await mintToken();
+            const [nft2, ata2] = await mintToken();
+            const [nft3, ata3] = await mintToken();
+
+            let mint1AccountMeta = createAccountMeta(nft1.address, nft1.metadataAddress, nft1.edition.address, ata1);
+            let mint2AccountMeta = createAccountMeta(nft2.address, nft2.metadataAddress, nft2.edition.address, ata2);
+            let mint3AccountMeta = createAccountMeta(nft3.address, nft3.metadataAddress, nft3.edition.address, ata3);
+
+            let combinedAccountMeta = [...mint1AccountMeta, ...mint2AccountMeta, ...mint3AccountMeta];
+
+            console.log("Runs \"burn\" ix...");
+            const burnIx = await program.methods.burn()
+                .accounts({
+                    authority: test_keypair.publicKey,
+                    feesReceiver: test_keypair.publicKey,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+                    systemProgram: anchor.web3.SystemProgram.programId
+                })
+                .remainingAccounts(combinedAccountMeta)
+                .signers([test_keypair])
+                .postInstructions([transferTicketIx])
+                .rpc({
+                    skipPreflight: true
+                });
+
+                console.log("Burn tx hash: ", burnIx);
+        } catch (error) {
+            console.log("An error occurred: ", error);
+        }
+    })
 });
